c4480baec95476bd4b0d8524f150b5332ada097b
Giudicelli Elias <86128363+giudicellisilva@users.noreply.github.com>
2023-12-12 12:14:01 -0300

    Mudança na lógica de criação do users

--- src/controllers/categoriasController.js
+++ src/controllers/categoriasController.js
@@ -1,11 +1,32 @@
-const categorias = [{id: 1, name: "Livros", description: "Categoria voltada para livros"}]
-function getAllCategorias(){
-    return categorias;
+const users = []
+async function getAllCategorias(name){
+    const user = await users.find((user) => user.name === name);
+    return user ? user.categorias : [];
 }
 
-async function setCategoria(Name, Description){
-    const newId = categorias.length + 1;
-    await categorias.push({id: newId, name: Name, description: Description});
+
+async function setCategoria(userName, Name, Description){
+    let newId = 1;
+    const userIndex = users.findIndex((user) => user.name === userName);
+    if (userIndex !== -1) {
+      // Adiciona a categoria ao usuário existente
+      newId = users[userIndex].categorias.length + 1
+      users[userIndex].categorias.push({
+        id: newId,
+        name: Name,
+        description: Description,
+      });
+    } else {
+      // Cria um novo usuário com a categoria
+      users.push({
+        name: userName,
+        categorias: [{
+          id: newId,
+          name: Name,
+          description: Description,
+        }],
+      });
+    }
     return {id: newId, name: Name, description: Description}
 }
 
--- src/routes/categorias.js
+++ src/routes/categorias.js
@@ -2,12 +2,12 @@ const express = require('express');
 const {getAllCategorias, setCategoria} = require('../controllers/categoriasController');
 const router = express.Router();
 
-router.get('/', function (req, res, next) {
-    res.status(200).send({data: getAllCategorias()});
+router.post('/', async function (req, res) {
+    res.status(200).json({"resposta" : await getAllCategorias(req.body.name) });
 });
 
-router.post("/", async function(req, res){
-    res.status(200).json({"resposta" : await setCategoria(req.body.name, req.body.description ) });
+router.post("/new", async function(req, res){
+    res.status(200).json({"resposta" : await setCategoria(req.body.userName, req.body.name, req.body.description ) });
 })
 
 module.exports = router;
